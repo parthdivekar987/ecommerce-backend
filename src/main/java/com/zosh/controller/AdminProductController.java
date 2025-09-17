167c8f7180673b88dc2a957383dd0a8737c31f09
parthdivekar987 <parthdivekar987@gmail.com>
2025-09-17 19:26:38 +0530

    udpdated for deleete functionality;

--- src/main/java/com/zosh/controller/AdminProductController.java
+++ src/main/java/com/zosh/controller/AdminProductController.java
@@ -1,5 +1,6 @@
 package com.zosh.controller;
 
+
 import com.zosh.exception.ProductException;
 import com.zosh.model.Product;
 import com.zosh.request.CreateProductRequest;
@@ -19,8 +20,6 @@ public class AdminProductController {
     @Autowired
     private ProductService productService;
 
-    // ... (Your other methods like createProduct, findAllProduct etc., remain the same) ...
-
     @PostMapping("/")
     public ResponseEntity<Product> createProduct (@RequestBody CreateProductRequest req) {
         Product product = productService.createProduct(req);
@@ -28,27 +27,25 @@ public class AdminProductController {
         return new ResponseEntity<Product>(product, HttpStatus.CREATED);
     }
 
-    // MODIFIED: This now uses the message returned from the service.
+
     @DeleteMapping("/{productId}/delete")
     public ResponseEntity<ApiResponse> deleteProduct(@PathVariable Long productId) throws ProductException {
-        // The service call will either succeed and return a message,
-        // or throw a ProductException which our GlobalExceptionHandler will catch.
-        String message = productService.deleteProduct(productId);
-
+        productService.deleteProduct(productId);
         ApiResponse res = new ApiResponse();
-        res.setMessage(message);
+        res.setMessage("product deleted Successfully");
         res.setStatus(true);
 
-        return new ResponseEntity<>(res, HttpStatus.OK);
+        return  new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Product>> findAllProduct() {
         List<Product> products = productService.findAllProducts();
-        return new ResponseEntity<>(products, HttpStatus.OK);
+
+        return new ResponseEntity<> (products, HttpStatus.OK);
+
     }
 
-    // ... (Rest of the file remains the same) ...
     @PutMapping("/{productId}/update")
     public ResponseEntity<Product> updateProduct ( @RequestBody Product req, @PathVariable Long productId) throws ProductException {
         Product product = productService.updateProduct(productId, req );
@@ -63,9 +60,18 @@ public class AdminProductController {
         }
 
         ApiResponse res = new ApiResponse();
-        res.setMessage("products created successfully");
+        res.setMessage("product deleted successfully");
         res.setStatus(true);
 
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+
     }
+
+
+
+
+
+
+
+
 }
